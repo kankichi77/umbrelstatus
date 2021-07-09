79033260b27d135b7ee2e4fdf048493f3330ad5c
kankichi77 <kan.hayashi@gmail.com>
2021-07-10 01:19:13 +0800

    Update reporttemp.sh

--- reporttemp.sh
+++ reporttemp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 id=`cat $1`
 d=`date`
-t="status"
+t=`vcgencmd measure_temp`
 curl -s --header "Content-Type: application/json"   --request POST   --data "{\"id\":\"$id\",\"ts\":\"$d\",\"temp\":\"$t\"}"   https://umbrel.kankichi.xyz/umbrelstatus.php
 
